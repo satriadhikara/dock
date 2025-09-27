bf688bd2fef5da82b43a616d5eacea9493be348b
Andhikafdh <andhikafadhillah2211@gmail.com>
2025-09-27 08:34:07 +0700

    refactor/login page

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -2,8 +2,11 @@ import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-[#1E609E] bg-[url('/loginBackground.png')] bg-cover bg-center bg-no-repeat flex min-h-svh flex-col items-end justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-xl flex-col gap-6">
+    <div className="relative bg-[#1E609E] flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="absolute inset-0 bg-[url('/loginBackground.png')] bg-cover bg-center bg-no-repeat opacity-50"></div>
+
+      {/* Content */}
+      <div className="relative z-10 flex w-full max-w-xl flex-col gap-6">
         <LoginForm />
       </div>
     </div>
